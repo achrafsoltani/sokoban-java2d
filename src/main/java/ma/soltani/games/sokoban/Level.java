b5c45748c7f1a5759f4a8ed45768e4175719c317
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-15 15:55:10 +0100

    JPanel must be used as a container for objects

--- src/main/java/ma/soltani/games/sokoban/Level.java
+++ src/main/java/ma/soltani/games/sokoban/Level.java
@@ -2,20 +2,29 @@ package ma.soltani.games.sokoban;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class Level extends JPanel
 {
+    private Image mario;
+
     public Level()
     {
         Dimension d = new Dimension();
         d.width = 800;
         d.height = 600;
         setPreferredSize(d);
+
+        ImageIcon IC = new ImageIcon(Objects.requireNonNull(getClass().getResource("/playerFace.png")));
+        this.mario = IC.getImage();
     }
 
     @Override
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(mario, 0, 0, null);
     }
 }
