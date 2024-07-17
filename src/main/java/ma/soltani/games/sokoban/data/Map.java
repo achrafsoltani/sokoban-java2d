a0667dba1df740c9880ecfb2df7848c371efd45c
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-17 15:52:08 +0100

    Fix map rendering issues

--- src/main/java/ma/soltani/games/sokoban/data/Map.java
+++ src/main/java/ma/soltani/games/sokoban/data/Map.java
@@ -1,14 +1,19 @@
 package ma.soltani.games.sokoban.data;
 
+import ma.soltani.games.sokoban.helpers.ImageLoader;
 import ma.soltani.games.sokoban.helpers.MapGenerator;
+
+import java.awt.*;
 
 public class Map
 {
     private Tile[][] tiles;
+    private int tileSize;
 
-    public Map()
+    public Map(int tileSize)
     {
-        Tile[][] tiles = MapGenerator.generateMap();
+        this.tiles = MapGenerator.generateMap();
+        this.tileSize = tileSize;
     }
 
     public Tile[][] getTiles() {
@@ -17,5 +22,18 @@ public class Map
 
     public void setTiles(Tile[][] tiles) {
         this.tiles = tiles;
+    }
+
+    public void render(Graphics g)
+    {
+        Image img = ImageLoader.loadImage("/Blocks/block_01.png");
+
+        for (int y = 0; y < 7; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                g.drawImage(img, tiles[y][x].getX() * this.tileSize, tiles[y][x].getY() * this.tileSize, null);
+            }
+        }
     }
 }
