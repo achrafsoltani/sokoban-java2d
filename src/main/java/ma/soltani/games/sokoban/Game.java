76849eb8835ad46f23e33196db0124e93c8b35bd
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-17 14:34:24 +0100

    Game class

---
+++ src/main/java/ma/soltani/games/sokoban/Game.java
@@ -0,0 +1,27 @@
+package ma.soltani.games.sokoban;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game extends JPanel implements ActionListener
+{
+    private Image block;
+    private Image crate;
+    private Image environment;
+    private Image ground;
+    private Image player;
+
+    public Game()
+    {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    // Helper methods
+}
