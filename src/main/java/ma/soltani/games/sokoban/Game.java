55ba308b4df7dfee3f16fdda810b08a244d3a7fe
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-17 15:58:58 +0100

    remove map logic and tiles from the game class

--- src/main/java/ma/soltani/games/sokoban/Game.java
+++ src/main/java/ma/soltani/games/sokoban/Game.java
@@ -14,17 +14,11 @@ public class Game extends JPanel implements ActionListener
     private final int MAP_HEIGHT = 7;
     private final int MAP_TILE_SIZE = 64;
 
-    private Image block;
-    private Image crate;
-    private Image environment;
-    private Image ground;
-    private Image player;
     private Map map;
 
     public Game()
     {
         setPreferredSize(new Dimension(MAP_WIDTH * MAP_TILE_SIZE, MAP_HEIGHT * MAP_TILE_SIZE));
-        this.loadImages();
         this.map = new Map(MAP_TILE_SIZE);
     }
 
@@ -38,15 +32,5 @@ public class Game extends JPanel implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e) {
 
-    }
-
-    // Helper methods
-    private void loadImages()
-    {
-        this.block = ImageLoader.loadImage("/Blocks/block_01.png");
-        this.crate = ImageLoader.loadImage("/Crates/crate_01.png");
-        this.environment = ImageLoader.loadImage("/Environment/environment_01.png");
-        this.ground = ImageLoader.loadImage("/Ground/ground_01.png");
-        this.player = ImageLoader.loadImage("/Player/player_01.png");
     }
 }
