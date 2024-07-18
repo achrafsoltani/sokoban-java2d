f465292a0964159b28f8f8c848739f526a128ddc
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-18 15:51:41 +0100

    Update the game class to render the player object

--- src/main/java/ma/soltani/games/sokoban/Game.java
+++ src/main/java/ma/soltani/games/sokoban/Game.java
@@ -1,7 +1,9 @@
 package ma.soltani.games.sokoban;
 
 import ma.soltani.games.sokoban.data.Map;
+import ma.soltani.games.sokoban.data.Player;
 import ma.soltani.games.sokoban.helpers.ImageLoader;
+import ma.soltani.games.sokoban.objects.Mario;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,17 +17,20 @@ public class Game extends JPanel implements ActionListener
     private final int MAP_TILE_SIZE = 64;
 
     private Map map;
+    private Player player;
 
     public Game()
     {
         setPreferredSize(new Dimension(MAP_WIDTH * MAP_TILE_SIZE, MAP_HEIGHT * MAP_TILE_SIZE));
         this.map = new Map(MAP_TILE_SIZE);
+        this.player = new Player();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.map.render(g);
+        this.player.render(g);
         Toolkit.getDefaultToolkit().sync();
     }
 
