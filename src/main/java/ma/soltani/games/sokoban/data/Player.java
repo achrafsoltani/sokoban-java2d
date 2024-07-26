de21d71da105994b037261d96053365dedcc72d4
Achraf Soltani <achraf.soltani@gmail.com>
2024-07-26 15:12:38 +0100

    Load all different states of player image

--- src/main/java/ma/soltani/games/sokoban/data/Player.java
+++ src/main/java/ma/soltani/games/sokoban/data/Player.java
@@ -6,7 +6,11 @@ import java.util.Objects;
 
 public class Player
 {
-    private Image image;
+    private Image image_down;
+    private Image image_up;
+    private Image image_right;
+    private Image image_left;
+    private Image selected;
     private int x;
     private int y;
     private int spriteSize;
@@ -16,18 +20,32 @@ public class Player
         this.x = x;
         this.y = y;
         this.spriteSize = spriteSize;
-        ImageIcon IC = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Player/player_01.png")));
-        this.image = IC.getImage();
+        ImageIcon down = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Player/player_01.png")));
+        ImageIcon up = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Player/player_02.png")));
+        ImageIcon right = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Player/player_16.png")));
+        ImageIcon left = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Player/player_11.png")));
+
+        this.image_down = down.getImage();
+        this.image_up = up.getImage();
+        this.image_right = right.getImage();
+        this.image_left = left.getImage();
+
+        this.selected = this.image_down;
     }
 
     public void render(Graphics g)
     {
-        g.drawImage(this.image, x*spriteSize, y*spriteSize, null);
+        g.drawImage(this.selected, x*spriteSize, y*spriteSize, null);
     }
 
     public void moveX(int dx)
     {
         this.x += dx;
+        if(dx>0){
+            this.selected = this.image_right;
+        } else {
+            this.selected = this.image_left;
+        }
     }
 
     public void moveY(int dy)
